081df94910cafca3ea7940a05b4d31f5f4e7f7e6
lexyking <sahiealexandre@hotmail.fr>
2019-08-10 00:36:26 +0100

    passport strategy

---
+++ passport.js
@@ -0,0 +1,9 @@
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
+
+passport.use(new JwtStrategy({
+
+}, async (payload, done) => {
+
+}))
