44fcee7831ab34eded80f94409fb8ad92346437a
lexyking <sahiealexandre@hotmail.fr>
2019-08-09 22:21:26 +0100

    created db

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true },
+  () => {
+    console.log("connected to db!");
+  })
+
 
 const app = express();
 
