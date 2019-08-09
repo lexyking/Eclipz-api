1bb07ec90e877328de90ad4e63a6e1d005a13893
lexyking <sahiealexandre@hotmail.fr>
2019-08-10 00:47:12 +0100

    find the user specified in the token

--- passport.js
+++ passport.js
@@ -1,9 +1,27 @@
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const { ExtractJwt } = require('passport-jwt');
+const dotenv = require('dotenv');
+const User = require('./models/user');
+
+dotenv.config();
 
 passport.use(new JwtStrategy({
-
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: process.env.SECRET_TOKEN
 }, async (payload, done) => {
+  try {
+    //Find the user specified in token
+    const user = await User.findById(payload.sub);
 
+    //If user doesn't exist, handle it
+    if (!user) {
+      return done(null, false);
+    }
+
+    //Otherwise, return the user
+    done(null, user);
+  } catch (error) {
+    done(error, false);
+  }
 }))
